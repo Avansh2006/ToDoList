594dcbfbd17e95df4fd8c83284fed6c4d9a93e59
Avansh2006 <avanshyadav20072006@gmail.com>
2024-09-20 00:10:52 +0530

    Final Commit

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
-let input = document.getElementById('todoInput') ;
+let input = document.getElementById('todoInput');
 let ul = document.querySelector('ul');
-document.getElementById('addButton').addEventListener('click',()=>{
-   let li = document.createElement('li');
-   li.innerText = input.value ;
-   if(input.value == ''){
-       alert("Please Enter a Task!!")
-    }
-    else{
-        ul.appendChild(li);
-        input.value ='' ;
-    }
-   
+alert("you can also press ENTER key to add a Task !!")
 
+document.getElementById('addButton').addEventListener('click', () => {
+    let li = document.createElement('li');
+    let delBtn = document.createElement('button');
+    delBtn.innerText = 'Delete';
+    li.innerText = input.value;
+    if (input.value == '') {
+        alert("Please Enter a Task!!")
+    }
+    else {
+        ul.appendChild(li);
+        li.append(delBtn)
+        delBtn.addEventListener('click', ()=>{
+            delBtn.parentElement.remove();
+        })
+        input.value = '';
+    }
+
+
+})
+document.addEventListener('keydown', function (e) {
+    if (e.key == 'Enter') {
+        let li = document.createElement('li');
+    let delBtn = document.createElement('button');
+    delBtn.innerText = 'Delete';
+    li.innerText = input.value;
+        if (input.value == '') {
+            alert("Please Enter a Task!!")
+        }
+        else {
+            ul.appendChild(li);
+        li.append(delBtn)
+        delBtn.addEventListener('click', ()=>{
+            delBtn.parentElement.remove();
+        })
+        input.value = '';
+        }
+    }
+    
 })
